22f0d6b1d5f562183446d300f188d94bc8ce8d1b
Taiful360 <taifulpersonal@gmail.com>
2023-08-19 17:23:59 +0600

    first commit

---
+++ js/areacalculator.js
@@ -0,0 +1,31 @@
+// Triangle Area
+function calculateTriangleArea() {
+  const baseField = document.getElementById("triangle-base");
+  const baseValueText = baseField.value;
+  const base = parseFloat(baseValueText);
+
+  const heightField = document.getElementById("triangle-height");
+  const heightValueText = heightField.value;
+  const height = parseFloat(heightValueText);
+
+  const area = 0.5 * base * height;
+
+  const areaSpan = document.getElementById("triangle-area");
+  areaSpan.innerText = area;
+}
+
+// Rectangle Area
+function calculateRectangleArea() {
+  const widthField = document.getElementById("rectangle-width");
+  const widthValueText = widthField.value;
+  const width = parseFloat(widthValueText);
+
+  const lengthField = document.getElementById("rectangle-length");
+  const lengthValueText = lengthField.value;
+  const length = parseFloat(lengthValueText);
+
+  const area = width * length;
+
+  const areaSpan = document.getElementById("rectangle-area");
+  areaSpan.innerText = area;
+}
